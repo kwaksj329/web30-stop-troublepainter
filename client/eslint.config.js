5983dfb34ca486b9aae9cebcdbe378d1df31c7a0
kwaksj329 <kwaksj329@gmail.com>
2024-11-02 17:04:08 +0900

    chore: Configure code formatter for client-side

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,28 +1,103 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import reactPlugin from 'eslint-plugin-react';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import importPlugin from 'eslint-plugin-import';
+import airbnb from 'eslint-config-airbnb-base';
+import airbnbTypeScript from 'eslint-config-airbnb-typescript';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+const airbnbConfig = {
+  plugins: {
+    ...airbnb.plugins,
+    ...airbnbTypeScript.plugins,
+    prettier: prettierPlugin,
+  },
+  rules: {
+    ...airbnb.rules,
+    ...airbnbTypeScript.rules,
+    'prettier/prettier': ['error'],
+    semi: ['error', 'always'],
+    '@typescript-eslint/semi': ['error', 'always'],
+  },
+};
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ['dist'],
+  },
+  {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.json', './tsconfig.app.json', './tsconfig.node.json'],
+      },
+      globals: {
+        ...globals.browser,
+      },
     },
     plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      react: reactPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
+      import: importPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      ...airbnbConfig.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/require-default-props': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
       ],
+
+      'import/prefer-default-export': 'off',
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'builtin',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          'newlines-between': 'never',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-misused-promises': 'error',
+
+      'jsx-a11y/anchor-is-valid': 'error',
+      'jsx-a11y/click-events-have-key-events': 'error',
     },
   },
-)
+  ...tseslint.configs.recommended,
+);

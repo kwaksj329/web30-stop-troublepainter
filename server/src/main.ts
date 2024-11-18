2992d8da302e42301795046b5043236b928d54aa
ssum1ra <101113206+ssum1ra@users.noreply.github.com>
2024-11-18 15:38:54 +0900

    feat: Enable CORS for all domain access

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://troublepainter-bucket.s3-website.kr.object.ncloudstorage.com',
+    origin: '*',
     methods: '*',
   });
   await app.listen(process.env.PORT ?? 3000);

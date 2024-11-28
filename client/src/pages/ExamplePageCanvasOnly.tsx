c462f783dd122e2ca8bc5f3a3091657fe1f1f8a3
kwaksj329 <kwaksj329@gmail.com>
2024-11-28 18:55:16 +0900

    fix: Add missing props to ExamplePageCanvasOnly component

--- client/src/pages/ExamplePageCanvasOnly.tsx
+++ client/src/pages/ExamplePageCanvasOnly.tsx
@@ -1,10 +1,10 @@
-import { PlayerRole } from '@troublepainter/core';
+import { PlayerRole, RoomStatus } from '@troublepainter/core';
 import { GameCanvas } from '@/components/canvas/GameCanvas';
 
 const ExamplePageCanvasOnly = () => {
   return (
     <div className="flex items-center justify-center">
-      <GameCanvas role={PlayerRole.DEVIL} />
+      <GameCanvas role={PlayerRole.DEVIL} currentRound={1} roomStatus={RoomStatus.DRAWING} />
     </div>
   );
 };
